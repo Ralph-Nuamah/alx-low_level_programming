ddc6507a0875184db4187675a4e0e41d30ac3f83
Ralph-Nuamah <boatengraphael3@gmail.com>
2023-06-19 15:05:38 -0700

    Inventing is a combination of brains and materials.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - A function that prints two digits
+* Return:0 (success)
+*/
+int main(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
